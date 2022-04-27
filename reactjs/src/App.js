2c14c4aac9986f37581cbdfaa8573fa59a66f176
Claudia <88203931+clauculus@users.noreply.github.com>
2022-04-27 11:51:53 +0700

    add template

---
+++ reactjs/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <body>
+     <div className="App">
+       <Header/>
+    </div>     
+    </body>
+  );
+}
+
+export default App;
---
+++ reactjs/src/component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <ul className="menu">
+        <li>Home</li>
+        <li>DNA Test</li>
+        <li>Searching</li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
