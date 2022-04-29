26433b55e8828f44c0552090c5356a137ddad963
Claudia <88203931+clauculus@users.noreply.github.com>
2022-04-29 22:45:19 +0700

    update searching handler

--- src/frontend/src/component/SearchBar.js
+++ src/frontend/src/component/SearchBar.js
@@ -4,15 +4,17 @@ import axios from 'axios';
 
 const SearchBar = () => {
   const [request, setRequest] = useState("");
-  const [data, setData] = useState("{}");
+  const [data, setData] = useState("{}"); // nerima data 
+  const [status, setStatus] = useState(""); // nerima status error
 
   const submitForm = async event => {
     event.preventDefault();
       
-    axios.get('http://localhost:8080/logs/' + encodeURI(request)).then((res) => setData(res.data)).catch((err) => {});
-
+    axios.get('http://localhost:8080/logs/' + encodeURI(request)).then((res) => {setData(res.data); setStatus(res.status);}).catch((err) => {setStatus(err.response.status);});
+    console.log(data);
+    console.log(status);
   }
-
+  // kalo res status 406 = Query invalid, res status 200 = data ada/engga 
   return (
     <div className="search">
         <div className="searchInput">
@@ -21,8 +23,9 @@ const SearchBar = () => {
         <div className='searchButton'>
           <input type = 'submit' value='Submit' className='btn-submit-searching' onClick = {submitForm}/>         
         </div>
-          {data !== "{}" ? data.map((d) => <Output text={d.tanggal + " - " + d.namaPasien + " - " + d.namaPenyakit + " - " + (d.kemiripan * 100).toFixed(2) + "% - " + JSON.stringify(d.hasil)}/>) : null }
-          {data == "" ? <Output text={"No data match the search"}/> : null}
+          {status === 406 ? <Output text={"Query invalid"} /> : 
+          (data.length !== 0 ? data.map((d) => <Output text={d.tanggal + " - " + d.namaPasien + " - " + d.namaPenyakit + " - " + (d.kemiripan * 100).toFixed(2) + "% - " + JSON.stringify(d.hasil)}/>) : 
+          (data == "" ? <Output text={"No data match the search"}/> : null))}
     </div>
   )
 }
