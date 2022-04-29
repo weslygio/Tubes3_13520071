996b6aef5a19c13bab4ba6913bc13fdbfdae73b6
Vieri <73509485+VieriMansyl@users.noreply.github.com>
2022-04-29 23:49:01 +0700

    Revert "update searching handler"
    This reverts commit 26433b55e8828f44c0552090c5356a137ddad963.

--- src/frontend/src/component/SearchBar.js
+++ src/frontend/src/component/SearchBar.js
@@ -4,17 +4,15 @@ import axios from 'axios';
 
 const SearchBar = () => {
   const [request, setRequest] = useState("");
-  const [data, setData] = useState("{}"); // nerima data 
-  const [status, setStatus] = useState(""); // nerima status error
+  const [data, setData] = useState("{}");
 
   const submitForm = async event => {
     event.preventDefault();
       
-    axios.get('http://localhost:8080/logs/' + encodeURI(request)).then((res) => {setData(res.data); setStatus(res.status);}).catch((err) => {setStatus(err.response.status);});
-    console.log(data);
-    console.log(status);
+    axios.get('http://localhost:8080/logs/' + encodeURI(request)).then((res) => setData(res.data)).catch((err) => {});
+
   }
-  // kalo res status 406 = Query invalid, res status 200 = data ada/engga 
+
   return (
     <div className="search">
         <div className="searchInput">
@@ -23,9 +21,8 @@ const SearchBar = () => {
         <div className='searchButton'>
           <input type = 'submit' value='Submit' className='btn-submit-searching' onClick = {submitForm}/>         
         </div>
-          {status === 406 ? <Output text={"Query invalid"} /> : 
-          (data.length !== 0 ? data.map((d) => <Output text={d.tanggal + " - " + d.namaPasien + " - " + d.namaPenyakit + " - " + (d.kemiripan * 100).toFixed(2) + "% - " + JSON.stringify(d.hasil)}/>) : 
-          (data == "" ? <Output text={"No data match the search"}/> : null))}
+          {data !== "{}" ? data.map((d) => <Output text={d.tanggal + " - " + d.namaPasien + " - " + d.namaPenyakit + " - " + (d.kemiripan * 100).toFixed(2) + "% - " + JSON.stringify(d.hasil)}/>) : null }
+          {data == "" ? <Output text={"No data match the search"}/> : null}
     </div>
   )
 }
